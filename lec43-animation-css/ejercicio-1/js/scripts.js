7474229deb95975c3758cafe7fb36bdc52b583f1
rolandoandre123 <rbardalespriale@gmail.com>
2023-07-13 07:09:18 -0500

    add : adding sping animation example

--- lec43-animation-css/ejercicio-1/js/scripts.js
+++ lec43-animation-css/ejercicio-1/js/scripts.js
@@ -1,5 +1,6 @@
 
 const button = document.getElementById("button");
+const circleLoad = document.getElementById('circle-load');
 const alerta = document.getElementById("alert");
 const root = document.documentElement;
 const timeAnimation = '1s';
@@ -7,8 +8,12 @@ const timeAnimation = '1s';
 root.style.setProperty('--timeAnimation', timeAnimation);
 
 button.addEventListener('click', () => {
-  alerta.classList.remove('add-animation');
+  alerta.classList.remove('animation-movealert');
+  circleLoad.classList.remove('hide');
+  circleLoad.classList.add('animation-rotatecircleload');
   setTimeout(() => {
-    alerta.classList.add('add-animation');
-  }, timeAnimation)
+    circleLoad.classList.remove('animation-rotatecircleload');
+    alerta.classList.add('animation-movealert');
+    circleLoad.classList.add('hide');
+  }, 2000);
 });
